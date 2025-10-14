888f9b09cdff68419eb4aff71cdd665ecdba6a29
YasSerHbh <hbhyasser@gmail.com>
2025-10-14 17:38:58 +0100

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+
+const Hero = () => {
+  return (
+    <section id="hero">
+        <div>
+            <h1>Macbook Pro</h1>
+            <img src="/title.png" alt="Macbook Title" />
+        </div>
+            <video src="/videos/hero.mp4" muted autoPlay playsInline></video>
+
+            <button>Buy Now</button>
+
+            <p>From $1999 or $177/mo for 12 months</p>
+    </section>
+  )
+}
+
+export default Hero
