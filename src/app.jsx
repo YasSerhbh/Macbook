07d9dee35b3d812d1ef76ac8f61da71c26e9ae53
YasSerHbh <hbhyasser@gmail.com>
2025-10-17 19:11:27 +0100

    features section

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import Showcase from './components/Showcase'
 import Performance from './components/Performance'
+import Features from './components/Features'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -17,6 +18,7 @@ const App = () => {
         <ProductViewer />
         <Showcase />
         <Performance />
+        <Features />
     </main>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,13 @@ import { create } from "zustand";
 export const useMacbookStore = create((set) => ({
 
     color: "#2E2C2E",
-
     setColor: (color) => set({ color }),
 
     scale: 0.08,
-
     setScale: (scale) => set({ scale }),
 
-    reset: () => set({ color: "#2E2C2E", scale: 0.08 }),
+    texture: "/videos/feature-1.mp4",
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({ color: "#2E2C2E", scale: 0.08, texture: "/videos/feature-1.mp4" }),
 }));
